d56e1ecf901149814a68d0e0327040cbf5d866b2
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-07 03:08:00 +0900

    chore: staged changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:tailwind_colors/tailwind_colors.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ห.ว.ย.',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Page(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class Page extends StatefulWidget {
+  Page({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  PageWithState createState() => PageWithState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PageWithState extends State<Page> {
+  // create controller for text input
+  final textInputController = TextEditingController();
+  FocusNode textInputFocus;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // init focus on mount
+  @override
+  void initState() {
+    super.initState();
+
+    textInputFocus = FocusNode();
+  }
+
+  // clean when removed
+  @override
+  void dispose() {
+    textInputController.dispose();
+    textInputFocus.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return GestureDetector(
+        onTap: () {
+          textInputFocus.unfocus();
+        },
+        child: Scaffold(
+            body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: 160),
+                  child: Column(children: [
+                    // title
+                    Text('ห.ว.ย.',
+                        style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: 'Prompt',
+                            fontWeight: FontWeight.w600)),
+                    // subtitle
+                    Text('ข้อมูลสลากกินแบ่งวันที่ 31 กุมภาพันธ์ 2564',
+                        style: TextStyle(fontSize: 16, fontFamily: 'Niramit')),
+                    // number input
+                    Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 24, horizontal: 36),
+                        child: Material(
+                            elevation: 20,
+                            borderRadius: BorderRadius.circular(6.0),
+                            shadowColor: Color.fromRGBO(0, 0, 0, 0.05),
+                            child: TextField(
+                                focusNode: textInputFocus,
+                                controller: textInputController,
+                                keyboardType: TextInputType.number,
+                                style: new TextStyle(fontSize: 16),
+                                decoration: InputDecoration(
+                                  fillColor: Colors.white,
+                                  focusColor: TWColors.blue.shade500,
+                                  contentPadding: new EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 12),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      borderSide: BorderSide(
+                                          color: TWColors.gray.shade200)),
+                                )))),
+                    // result
+                    Padding(
+                        padding: EdgeInsets.only(top: 16, left: 36, right: 36),
+                        child: Material(
+                            elevation: 2,
+                            borderRadius: BorderRadius.circular(8.0),
+                            shadowColor: Color.fromRGBO(0, 0, 0, 0.5),
+                            child: Container(
+                                child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 16),
+                                    child: Column(children: [
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.check_circle,
+                                            color: TWColors.green.shade500,
+                                            size: 32,
+                                            semanticLabel: 'Check',
+                                          ),
+                                          SizedBox(width: 8),
+                                          Text('ยินดีด้วย!',
+                                              style: TextStyle(
+                                                  fontFamily: 'Prompt',
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20)),
+                                        ],
+                                      ),
+                                      Padding(
+                                          padding: EdgeInsets.only(
+                                              top: 8, left: 6, right: 6),
+                                          child: Text(
+                                              'ขอแสดงความยินดีด้วย! เลขสลากกินแบ่งของคุณได้รับรางวัลที่ 1',
+                                              style: TextStyle(
+                                                  fontFamily: 'Niramit',
+                                                  fontSize: 14)))
+                                    ])))))
+                  ])),
+            ],
+          ),
+        )));
   }
 }
